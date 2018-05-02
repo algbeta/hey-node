5dbf42f3018599598244b1d59dfe7c526c6fce9e
Natallia Gorbunova <gorbunovano@gmail.com>
2018-05-02 17:13:31 +0200

    put settings into config

--- src/dirwatch/importer.js
+++ src/dirwatch/importer.js
@@ -1,19 +1,18 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as csvjson from 'csvjson';
-
-const encoding = 'utf8';
+import * as config from '../config';
 
 class Importer {
   import(pathToFile) {
     return new Promise((resolve, reject) => {
-      fs.readFile(path.join(__dirname, pathToFile), encoding, (err, data) => {
+      fs.readFile(path.join(__dirname, pathToFile), config.encoding, (err, data) => {
         if (err) {
           reject(err);
         } else {
           try {
             const json = csvjson.toObject(data, {
-              delimiter: ','
+              delimiter: config.delimiter
             });
             resolve(json);
           } catch (convertErr) {
@@ -26,9 +25,9 @@ class Importer {
 
   importSync(pathToFile) {
     try {
-      const data = fs.readFileSync(path.join(__dirname, pathToFile), { encoding });
+      const data = fs.readFileSync(path.join(__dirname, pathToFile), { encoding: config.encoding });
       return csvjson.toObject(data, {
-        delimiter: ','
+        delimiter: config.delimiter
       });
     } catch (err) {
       console.log(err.message);
