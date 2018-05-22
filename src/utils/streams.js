649112a6e8c53df8e893cf39cdba91aeb60e3ac2
Natallia <gorbunovano@gmail.com>
2018-05-23 00:37:54 +0200

    css builder

--- src/utils/streams.js
+++ src/utils/streams.js
@@ -4,6 +4,16 @@ const commander = require('commander');
 const fs = require('fs');
 const csvjson = require('csvjson');
 
+// aux functions
+function printHelpMessage() {
+  console.log('[ERROR] incorrect input');
+  commander.help();
+}
+
+function errorHandler(err) {
+  console.log(`[ERROR] ${err.message}`);
+}
+
 /**
  * reverse implementation
  * reads console input
@@ -14,8 +24,8 @@ function reverse() {
   process.stdin.resume();
   process.stdout.write('Please, enter a string: ');
 
-  process.stdin.once('data', data => {
-    if (data !== null) {
+  process.stdin.on('data', data => {
+    if (data && data.trim()) {
       const finalData = data
         .toString()
         .trim()
@@ -23,7 +33,7 @@ function reverse() {
         .reverse()
         .join('');
       process.stdout.write(`data: ${finalData}`);
-      process.stdin.end();
+      process.exit();
     }
   });
 }
@@ -31,9 +41,9 @@ function reverse() {
 /**
  *  transform implemenation
  */
-function write(buffer, enconding) {
+function write(buffer) {
   if (buffer) {
-    const data = buffer.toString(enconding);
+    const data = buffer.toString();
     this.push(`data: ${data.toUpperCase()}`);
   }
   process.exit();
@@ -45,15 +55,19 @@ function end(done) {
 function transform() {
   const stream = through(write, end);
   process.stdout.write('Please, enter a string: ');
-  process.stdin.pipe(stream).pipe(process.stdout);
+  process.stdin
+    .pipe(stream)
+    .pipe(process.stdout)
+    .on('error', errorHandler);
 }
 /**
  * outputFile implementation
  * @param {string} file: name of file to be displayed in console
  */
 function outputFile(file) {
-  const reader = fs.createReadStream(file);
-  reader.pipe(process.stdout);
+  const reader = fs.createReadStream(path.join(__dirname, file));
+  reader.on('error', errorHandler);
+  reader.pipe(process.stdout).on('error', errorHandler);
 }
 /**
  * convertFromFile implementation
@@ -61,31 +75,58 @@ function outputFile(file) {
  */
 function convertFromFile(file) {
   const reader = fs.createReadStream(path.join(__dirname, file));
+  reader.on('error', errorHandler);
   const toObject = csvjson.stream.toObject();
-  var stringify = csvjson.stream.stringify();
-  reader.pipe(toObject).pipe(stringify).pipe(process.stdout);
+  const stringify = csvjson.stream.stringify();
+  reader
+    .pipe(toObject)
+    .pipe(stringify)
+    .pipe(process.stdout)
+    .on('error', errorHandler);
 }
 /**
- * convertToFile
+ * convertToFile implementation
  * @param {string} file : name of csv file to be saved to json
  */
 function convertToFile(file) {
   const reader = fs.createReadStream(path.join(__dirname, file));
+  reader.on('error', errorHandler);
   const jsonName = file.replace('.csv', '');
   const writer = fs.createWriteStream(path.join(__dirname, `${jsonName}.json`));
+  writer.on('error', errorHandler);
   const toObject = csvjson.stream.toObject();
   const stringify = csvjson.stream.stringify();
-  reader.pipe(toObject).pipe(stringify).pipe(writer);
+  reader
+    .pipe(toObject)
+    .pipe(stringify)
+    .pipe(writer)
+    .on('error', errorHandler);
 }
+/**
+ * cssBuilder implementation
+ * @param {*} pathToDir - path to directory to be read
+ */
+function cssBuilder(pathToDir) {
+  const finalPathToDir = path.join(__dirname, pathToDir);
+  const writer = fs.createWriteStream(
+    path.join(__dirname, pathToDir, `${pathToDir}.css`)
+  );
+  writer.on('error', errorHandler);
+  fs.readdir(finalPathToDir, {}, (err, files) => {
+    if (err) errorHandler(err);
 
-function printHelpMessage() {
-  console.log('[ERROR] incorrect input');
-  commander.help();
+    files.forEach(file => {
+      const reader = fs.createReadStream(path.join(__dirname, pathToDir, file));
+      reader.on('error', errorHandler);
+      reader.pipe(writer).on('error', errorHandler);
+    });
+  });
 }
 
 commander
   .option('-a, --action', 'run an action')
   .option('-f, --file', 'set path to a file')
+  .option('-p, --path', 'path to a directory used by cssBuilder')
   .action((action, file) => {
     switch (action) {
       case 'reverse': {
@@ -97,15 +138,35 @@ commander
         break;
       }
       case 'outputFile': {
+        if (!commander.file) {
+          printHelpMessage();
+          break;
+        }
         outputFile(file);
         break;
       }
       case 'convertFromFile': {
+        if (!commander.file) {
+          printHelpMessage();
+          break;
+        }
         convertFromFile(file);
         break;
       }
       case 'convertToFile': {
+        if (!commander.file) {
+          printHelpMessage();
+          break;
+        }
         convertToFile(file);
+        break;
+      }
+      case 'cssBuilder': {
+        if (!commander.path) {
+          printHelpMessage();
+          break;
+        }
+        cssBuilder(file);
         break;
       }
       default:
