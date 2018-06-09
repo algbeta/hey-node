d36d8311357a9d1d3a3a50de98e669c49c1f9bcc
Natallia <gorbunovano@gmail.com>
2018-06-09 16:48:26 +0200

    html server implementation

--- src/http-servers/html-server.js
+++ src/http-servers/html-server.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 
 require('http')
   .createServer()
   .on('request', (req, res) => {
     try {
-      const data = fs.readFileSync(path.join(__dirname, 'index.html'));
+      let data = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
+      const urlObj = url.parse(req.url, true);
+      if (urlObj && urlObj.query && urlObj.query.message) {
+        data = data.replace('{message}', urlObj.query.message);
+      }
       res.writeHead(200, {
         'Content-Type': 'text/html'
       });
