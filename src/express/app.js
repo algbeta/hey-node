2e40a41a822d232cc5aefb177c5a99576ae77115
Natallia <gorbunovano@gmail.com>
2018-06-17 14:51:49 +0200

    jwt token implementation

--- src/express/app.js
+++ src/express/app.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
 
-const cookieMiddleware = require('./middlewares/cookie-parser');
-const queryMiddleware = require('./middlewares/query-parser');
 const routes = require('./routes');
 
 app.use(express.json());
-app.use(cookieMiddleware);
-app.use(queryMiddleware);
 app.use('/', routes);
 
 module.exports = app;
