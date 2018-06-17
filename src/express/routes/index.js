0991a7fc2c75f6cc4c8be89dce5385299fbd6bd3
Natallia <gorbunovano@gmail.com>
2018-06-18 00:19:59 +0200

    facebook and twitter passport auth strategies

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -1,7 +1,6 @@
-const express = require('express');
 const jwt = require('jsonwebtoken');
+const path = require('path');
 const uuidv1 = require('uuid/v1');
-const router = express.Router();
 const userData = require('../../../data/users.json');
 const config = require('../../config');
 const verifyTokenMiddleware = require('../middlewares/verify-token');
@@ -50,80 +49,103 @@ const data = {
   ]
 };
 
-router.post('/auth', (req, res) => {
-  const { login, password } = req.body;
-  const user = userData.users.find(
-    item => item.login === login && item.password === password
+module.exports = (router, passport) => {
+  router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+  });
+
+  router.post('/auth', (req, res) => {
+    const { login, password } = req.body;
+    const user = userData.users.find(
+      item => item.login === login && item.password === password
+    );
+
+    if (user) {
+      const payload = { userId: user.id };
+      const token = jwt.sign(payload, config.secret, { expiresIn: 1000 });
+
+      res.json({
+        code: 200,
+        message: 'OK',
+        data: {
+          user: {
+            email: user.email,
+            username: user.username
+          }
+        },
+        token
+      });
+    } else {
+      res.status(404).send({
+        code: 404,
+        message: 'Not Found'
+      });
+    }
+  });
+
+  router.get(
+    '/auth/facebook',
+    passport.authenticate('facebook')
   );
 
-  if (user) {
-    const payload = { userId: user.id };
-    const token = jwt.sign(payload, config.secret, { expiresIn: 1000 });
+  router.get(
+    '/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      successRedirect: '/',
+      failureRedirect: '/'
+    })
+  );
 
+  router.get('/auth/twitter', passport.authenticate('twitter'));
+
+  router.get('/auth/twitter/callback', )
+
+  router.get('/users', verifyTokenMiddleware, (req, res) => {
     res.json({
-      code: 200,
-      message: 'OK',
-      data: {
-        user: {
-          email: user.email,
-          username: user.username
-        }
-      },
-      token
+      users: data.users
     });
-  } else {
-    res.status(404).send({
-      code: 404,
-      message: 'Not Found'
-    });
-  }
-});
-
-router.get('/users', verifyTokenMiddleware, (req, res) => {
-  res.json({
-    users: data.users
   });
-});
 
-router.get('/products', verifyTokenMiddleware, (req, res) => {
-  res.json({
-    products: data.products
-  });
-});
-
-router.post('/products', (req, res) => {
-  const product = req.body;
-  if (!product) {
-    res.sendStatus('400');
-  } else {
-    product.id = uuidv1();
-    data.products.push(product);
-    res.json(product);
-  }
-});
-
-router.get('/products/:id', verifyTokenMiddleware, (req, res) => {
-  const product = data.products.find(item => item.id == req.params.id); // eslint-disable-line eqeqeq
-  if (!product) {
-    res.send({});
-  } else {
-    res.send(product);
-  }
-});
-
-router.get('/products/:id/reviews', verifyTokenMiddleware, (req, res) => {
-  const reviews = data.reviews.filter(item => item.productId == req.params.id); // eslint-disable-line eqeqeq
-  if (!reviews) {
-    res.send({});
-  } else {
-    res.send({
-      reviews
+  router.get('/products', verifyTokenMiddleware, (req, res) => {
+    res.json({
+      products: data.products
     });
-  }
-});
+  });
 
-router.get('*', (req, res) => {
-  res.sendStatus(404);
-});
+  router.post('/products', (req, res) => {
+    const product = req.body;
+    if (!product) {
+      res.sendStatus('400');
+    } else {
+      product.id = uuidv1();
+      data.products.push(product);
+      res.json(product);
+    }
+  });
 
-module.exports = router;
+  router.get('/products/:id', verifyTokenMiddleware, (req, res) => {
+    const product = data.products.find(item => item.id == req.params.id); // eslint-disable-line eqeqeq
+    if (!product) {
+      res.send({});
+    } else {
+      res.send(product);
+    }
+  });
+
+  router.get('/products/:id/reviews', verifyTokenMiddleware, (req, res) => {
+    const reviews = data.reviews.filter(
+      item => item.productId == req.params.id
+    ); // eslint-disable-line eqeqeq
+    if (!reviews) {
+      res.send({});
+    } else {
+      res.send({
+        reviews
+      });
+    }
+  });
+
+  router.get('*', (req, res) => {
+    res.sendStatus(404);
+  });
+};
