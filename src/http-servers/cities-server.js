f066e7cf8f55a64ce3783e77b83dfee9c1816aed
Natallia <gorbunovano@gmail.com>
2018-07-01 17:49:29 +0200

    random city server with mongoose

--- src/http-servers/cities-server.js
+++ src/http-servers/cities-server.js
@@ -1,28 +1,24 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017';
 const dbName = 'task7';
-let db = null;
+mongoose.connect(`${url}/${dbName}`);
 
-MongoClient.connect(
-  url,
-  (err, client) => {
-    if (!err) {
-      db = client.db(dbName);
-    }
+const Schema = mongoose.Schema;
+
+const CitySchema = new Schema({
+  name: String,
+  country: String,
+  capital: Boolean,
+  location: {
+    lat: Number,
+    long: Number
   }
-);
+});
+
+const CityModel = mongoose.model('City', CitySchema);
 
 const getRandomCity = (callback) => {
-  if (!db) {
-    return {};
-  }
-
-  const cities = db.collection('cities');
-  if (!cities) {
-    return {};
-  }
-
-  cities.find({}).toArray().then((allCitiesArray) => {
+  CityModel.find({}, (err, allCitiesArray) => {
     const randomIndex = Math.floor(Math.random() * allCitiesArray.length);
     callback(allCitiesArray[randomIndex]);
   });
@@ -39,9 +35,10 @@ require('http')
         res.end(JSON.stringify(result));
       });
     } catch (err) {
+      console.log(err.message);
       res.statusCode = 500;
       res.statusMessage = 'Server error occurred';
-      res.end();
+      res.end('An error occurred. Please, try later');
     }
   })
   .listen(3000);
