8412b8959213c4b34f37db0d1de98afb0a634b84
Natallia <gorbunovano@gmail.com>
2018-07-01 18:36:22 +0200

    added mongoose models for User and Product

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,5 +1,13 @@
-export class Product {
-  constructor() {
-    console.log('Product module');
-  }
-}
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  name: {
+    name: String,
+    required: [true, 'Product name is required!']
+  },
+  type: { type: String, required: [true, 'Product type is required!'] }
+});
+
+const ProductModel = mongoose.model('Product', ProductSchema);
+module.exports = ProductModel;
--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,17 @@
-export class User {
-  constructor() {
-    console.log('User module');
-  }
-}
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    name: String,
+    required: [true, 'User name is required!']
+  },
+  login: {
+    name: String,
+    required: [true, 'Login is required!']
+  },
+  password: { type: String, required: [true, 'Password is required!'] }
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+module.exports = UserModel;
